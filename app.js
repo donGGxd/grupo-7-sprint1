e99b14e8b610c9b3da3f5c92fa1acac0a093eb87
Alilucasx <alilucasx@gmail.com>
2023-08-07 16:53:37 -0300

    paginas de registro y inicio de sesion en proceso,footer terminado

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"))
 });
+app.get('/formulario',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/formulario.html"))
+});
+app.get('/a',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/a.html"))
+});
 
 app.use(express.static('Public'));
 
