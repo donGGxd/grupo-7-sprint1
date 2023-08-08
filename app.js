fb6e844f277dbe2331ed56a555443c1b8e3e99f4
Alilucasx <alilucasx@gmail.com>
2023-08-07 23:52:03 -0300

    diferentes tipos de zapatillas añadidas

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ app.get('/a',(req,res)=>{
 app.get('/producto1',(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/producto1.html"))
 });
+app.get('/producto2',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/producto2.html"))
+});
+app.get('/producto3',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/producto3.html"))
+});
+app.get('/producto4',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/producto4.html"))
+});
+app.get('/producto5',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/producto5.html"))
+});
+app.get('/producto6',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/producto6.html"))
+});
+app.get('/producto7',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/producto7.html"))
+});
 app.get('/carrito',(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/carrito.html"))
 });
