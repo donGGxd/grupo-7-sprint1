e69cdaf7194057f04fbf257c1c660955b74d6011
Alilucasx <alilucasx@gmail.com>
2023-09-03 20:12:59 -0300

    error arreglado

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const fs = require('fs'); 
 const productsFilePath = path.join(__dirname, '../data/ProductosDataBaseJ.json');
-const productsDescuenoFilePath = path.join(__dirname, '../data/ProductosDescuento.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-const productsDescuento = JSON.parse(fs.readFileSync(productsDescuenoFilePath, 'utf-8'));
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));  
+
+
+const productsDescuentoFilePath = path.join(__dirname, '../data/ProductosDescuento.json');
+const productsDescuento = JSON.parse(fs.readFileSync(productsDescuentoFilePath, 'utf-8'));
+
+
+
 const controlador = {
     inicio: (req, res) => {
-        res.render('home', { products: products },{productsDescuento:productsDescuento});
+        res.render('home', { products: products ,productsDescuento:productsDescuento});
     },
     formularioA: (req, res) => {
         res.render('a');
