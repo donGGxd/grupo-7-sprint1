3a482b7a5e6286e064d1d5f6f2b3b3ee5b5588b0
Alilucasx <alilucasx@gmail.com>
2024-01-13 06:52:07 -0300

    cambios añadidos

---
+++ database/models/zapatillas.js
@@ -0,0 +1,37 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const Zapatilla = sequelize.define('Zapatilla', {
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+      name: {
+        type: DataTypes.STRING,
+      },
+      precio: {
+        type: DataTypes.INTEGER,
+      },
+      marca:{
+        type:DataTypes.STRING
+      },
+      talla:{
+        type:DataTypes.INTEGER
+      },
+      cliente_id:{
+        type:DataTypes.INTEGER
+      }
+    },{
+        tableName:'Zapas',
+        timestamps:false
+    });
+    Zapatilla.associate=function(models){
+       Zapatilla.belongsTo(models.Clientes,{
+            as:'zapatillas',
+            foreignKey:'zapas_id'
+        })
+      }
+  
+    return Zapatilla ;
+  };
+  
